9df5e6cebbac80b2e34f005f5ffde39dce615d64
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-13 15:03:07 -0300

    CAMBIAR A PUERTO 5024

--- MiProyecto/Program.cs
+++ MiProyecto/Program.cs
@@ -21,6 +21,7 @@ namespace MiProyecto
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:5024");
                 });
     }
 }
